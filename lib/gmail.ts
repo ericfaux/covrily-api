5021a860d6c5c81b84f6ba3f3d3747bd601f9d85
ericfaux <67875487+ericfaux@users.noreply.github.com>
2025-09-15 21:41:16 -0400

    Expand Gmail OAuth scopes

--- lib/gmail.ts
+++ lib/gmail.ts
@@ -5,7 +5,10 @@ const CLIENT_ID = process.env.GMAIL_CLIENT_ID || "";
 const CLIENT_SECRET = process.env.GMAIL_CLIENT_SECRET || "";
 const REDIRECT_URI = process.env.GMAIL_REDIRECT_URI || "";
 
-const SCOPES = ["https://www.googleapis.com/auth/gmail.modify"];
+const SCOPES = [
+  "https://www.googleapis.com/auth/gmail.modify",
+  "https://www.googleapis.com/auth/gmail.labels",
+];
 
 function oauthClient() {
   return new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
