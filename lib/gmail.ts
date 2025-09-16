3cfe23842b308de17f101c7b40c4ac6eb7341cba
ericfaux <67875487+ericfaux@users.noreply.github.com>
2025-09-15 21:23:15 -0400

    Update Gmail scopes to allow modify access

--- lib/gmail.ts
+++ lib/gmail.ts
@@ -5,7 +5,7 @@ const CLIENT_ID = process.env.GMAIL_CLIENT_ID || "";
 const CLIENT_SECRET = process.env.GMAIL_CLIENT_SECRET || "";
 const REDIRECT_URI = process.env.GMAIL_REDIRECT_URI || "";
 
-const SCOPES = ["https://www.googleapis.com/auth/gmail.readonly"];
+const SCOPES = ["https://www.googleapis.com/auth/gmail.modify"];
 
 function oauthClient() {
   return new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
